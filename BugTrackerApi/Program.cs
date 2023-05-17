dd8c131e0a1eca38dbdb89b6cc106c721103e4ea
Justin Truong <satruongjustin@gmail.com>
2023-05-16 19:01:48 -0500

    removed some unnecessary files

--- BugTrackerApi/Program.cs
+++ BugTrackerApi/Program.cs
@@ -8,6 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddDbContext<BugEntityContext>(option => option.UseInMemoryDatabase("BugsListDb"));
+
+// Planning to replace in memory database with SQL Server database
+//builder.Services.AddDbContext<BugEntityContext>(option => option.UseSqlServer("Bugs.db"));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
